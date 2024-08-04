b13c1c8bf966fda054bf2c4bba306e71d978f226
Malvin King <malvink003@gmail.com>
2024-08-04 14:20:05 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# Malvin-Session-Generator
+- Kindly star my repo
+- Fork and edit as you wish
+- Deploy to your favourite hosting server eg Heroku or Render or self hosting
+
+<strong>NB:<strong/> This repo also generates session ID for all bots using whiskeysockets/baileys
+[![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#table-of-contents)
+<br/>QR- WEB - PAIR CODE FOR BOT WITH WHISKEYSOCKETS/BAILEYS
+[![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#table-of-contents)
+<p align="center">
+   <a href="https://github.com/kingmalvn">
+    <img src="https://telegra.ph/file/dc73e16b9988c7c56b56f.jpg" width="500">
+     
+</a>
+ <p align="center"><img src="https://profile-counter.glitch.me/{kingmalvn}/count.svg" alt="xcelsama:: Visitor's Count" /></p>
+
+
+
+[`ℹ️Contact Owner`](https://wa.me/263714757857)
+
+FORK THE REPOSITORY (Repo) 
+    <br>
+<a href="https://github.com/kingmalvn/malvin.session"><img title="WEB" src="https://img.shields.io/badge/FORK Malvin-QR?color=green&style=for-the-badge&logo=stackshare"></a>
+
+Now Deploy
+    <br>
+<a href='https://dashboard.heroku.com/new?template=https://github.com/kingmalvn/SESSION-GENERATOR' target="_blank"><img alt='DEPLOY' src='https://img.shields.io/badge/-DEPLOY-blue?style=for-the-badge&logo=heroku&logoColor=white'/>
+
+[`HERE'S AN EXAMPLE OUTPUT`](https://wasi-session-test-2d5de70f8522.herokuapp.com)
+# `Owner`
+
+ <a href="https://github.com/kingmalvn"><img src="https://github.com/kingmalvn.png" width="250" height="250" alt=" Malvin King"/></a>
+
---
+++ pair.js
@@ -0,0 +1,100 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: Itachi Uchiha,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+        async function GIFTED_MD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/'+id)
+     try {
+            let Pair Cod_By_Itachi_Uchiha = Itachi_Uchiha({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: ["Chrome (Linux)", "", ""]
+             });
+             if(!Pair_Code_By_Uchiha_Clan.authState.creds.registered) {
+                await delay(1500);
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await Pair_Code_By_Itachi_Uchiha.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            Pair_Code_By_Itachi_Uchiha.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Itachi_Uchiha.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(5000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(800);
+               let b64data = Buffer.from(data).toString('base64');
+               let session = await Pair_Code_By_Itachi_Uchiha.sendMessage(Pair_Code_By_Itachi_Uchiha.user.id, { text: '' + b64data });
+
+               let ITACHI_UCHIHA_TEXT = `
+*_Pair Code Connected by Malvin Tech*
+*_Made With 🤍_*
+______________________________________
+╔════◇
+║ *『 WOW YOU'VE CHOSEN Kings CLAN 』*
+║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
+╚════════════════════════╝
+╔═════◇
+║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║❒ *Ytube:* _youtube.com/@MalvinKing_TECH 
+║❒ *Owner:* _https://wa.me/263714757857
+║❒ *Repo:* _https://github.com/kingmalvn 
+║❒ *WaGroup:* _https://chat.whatsapp.com/C6mhOzGQqK5Lpu3y7noTOd_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029Vac8SosLY6d7CAFndv3Z_
+║❒ *Plugins:* _https://github.com/kingmalvn
+╚════════════════════════╝
+_____________________________________
+
+_Don't Forget To Give Star To My Repo_`
+ await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_ITACHI_UCHIHA.user.id,{text:UCHIHA_CLAN_TEXT},{quoted:session})
+ 
+
+        await delay(100);
+        await Pair_Code_By_ITACHI_UCHIHA.ws.close();
+        return await removeFile('./temp/'+id);
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    ITACHI_UCHIHA_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/'+id);
+         if(!res.headersSent){
+            await res.send({code:"Service Unavailable"});
+         }
+        }
+    }
+    return await UCHIHA_CLAN_PAIR_CODE()
+});
+module.exports = router
