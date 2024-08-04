9a529a152b09eef05b782dffa6d7a26783dc5634
Malvin King <malvink003@gmail.com>
2024-08-04 15:12:17 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: Itachi Uchiha,
+    default: 'Itachi Uchiha',
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
